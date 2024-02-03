c6025beafc0735e4cf86efc4314a02623f682801
Karthik Raj <51468149+KrajShuffle@users.noreply.github.com>
2024-02-03 13:23:40 -0800

    Added image_capture python script

---
+++ flor_image_emiss.py
@@ -0,0 +1,40 @@
+import datetime
+import sys
+from gpiozero import LED
+from picamera import PiCamera
+from time import sleep
+from fractions import Fraction
+
+"""
+Run file with python flor_image_emiss.py LED_WV
+
+Potential Values of LED_WV: 490 or 770 (refers to potential excitation wavelengths used, corresponding to fluorophore)
+
+"""
+
+# Creating Unique filename based on the time the image was captured
+cur_time = datetime.datetime.now()
+cur_time_str = cur_time.strftime("%m%d%H%M%S_IR")
+
+# Initialize Camera with proper settings
+camera = PiCamera(framerate = Fraction(1,6))
+camera.resolution = (1280,768) # (1280,768) Max on Laptop as of 3/13/2022
+camera.shutter_speed = 400_000 # max of 6 seconds of exposure (manipulate first & in microseconds);
+camera.iso = 100  # Works with 100, better to manipulate 2nd since it determines light sensitivity, amplifies image signal
+camera.rotation = 180
+sleep(30)
+camera.exposure_mode = 'off'
+
+# Initializing Specific Excitation LED
+if (sys.argv[1] == 770):
+    led_ex = LED(3) # 770 nm LED (LICOR)
+else:
+    led_ex = LED(18) # 490 nm LED (FAM)
+
+# Code for Excitation & Image Capture of Fluorophore Emission
+camera.start_preview()
+led_ex.on()
+camera.capture('/home/pi/Pictures/%s.png' % cur_time_str)
+camera.stop_preview()
+led_ex.off()
+camera.close()
